c026f2a2f8a9f48d0f12e4f71068aa3ae3dd171f
404Navdeep <navdeep13dps@gmail.com>
2026-01-05 19:59:27 +0530

    chnaged myself

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Im Navdeep, a coder, buisness nerd and a little bit of writer.
+        Im Navdeep, a tech connoisseur, entrepreneur and writer.
         </h2>
       </div>
     </div>
